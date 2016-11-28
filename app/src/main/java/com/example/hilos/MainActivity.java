aa49af22bb3efda45b50c6d42e7a891ae17ac8f4
Rcorvo <rcu1987@gmail.com>
2016-11-28 23:06:15 +0100

    Actividad 5.2 terminada

--- app/src/main/java/com/example/hilos/MainActivity.java
+++ app/src/main/java/com/example/hilos/MainActivity.java
@@ -24,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
     public void calculaOperacion(View view){
         int n = Integer.parseInt(entrada.getText().toString());
         salida.append(n + "! =");
-        int res = factorial(n);
-        salida.append(res + "\n");
+        MiThread thread = new MiThread(n);
+        thread.start();
     }
 
     public int factorial(int n){
@@ -35,5 +35,25 @@ public class MainActivity extends AppCompatActivity {
             SystemClock.sleep(1000);
         }
         return res;
+    }
+
+    class MiThread extends Thread{
+
+        private int n, res;
+
+        public MiThread(int n){
+            this.n = n;
+        }
+
+        @Override
+        public void run(){
+            res = factorial(n);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    salida.append(res + "\n");
+                }
+            });
+        }
     }
 }
